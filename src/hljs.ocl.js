a99e8e7510459833ea9013a22137acf49ba1f7bd
Stephan Köninger <github@stekoe.de>
2018-06-09 18:20:42 +0200

    chore(hljs): add endif as keyword

--- src/hljs.ocl.js
+++ src/hljs.ocl.js
@@ -1,7 +1,7 @@
 module.exports = function(hljs) {
     return {
         case_insensitive: true, // language is case-insensitive
-        keywords: 'and body context def derive else endif endpackage false if implies in init inv invalid let not null or package post pre self static then true xor',
+        keywords: 'endif and body context def derive else endif endpackage false if implies in init inv invalid let not null or package post pre self static then true xor',
         literal: 'false true null',
         contains: [
             {
